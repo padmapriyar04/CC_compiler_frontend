376396b481249d77cb7973b92a3f6346a6965d94
padmapriyar04 <ae22b040@smail.iitm.ac.in>
2024-01-19 09:09:15 +0530

    frontend files uploaded

---
+++ ide_frontend/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, useRef } from 'react';
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/mode-c_cpp';
+import 'ace-builds/src-noconflict/mode-typescript';
+import 'ace-builds/src-noconflict/theme-dreamweaver';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [language, setLanguage] = useState('py');
+  const [code, setCode] = useState('');
+  const editorRef = useRef();
+
+  useEffect(() => {
+    if (editorRef.current) {
+      const aceEditor = editorRef.current.editor;
+      if (language === 'js') {
+        aceEditor.getSession().setMode('ace/mode/javascript');
+      } else if (language === 'cpp') {
+        aceEditor.getSession().setMode('ace/mode/c_cpp');
+      } else if (language === 'py') {
+        aceEditor.getSession().setMode('ace/mode/python');
+      }else if(language === 'ts'){
+        aceEditor.getSession().setMode('ace/mode/typescript');
+      }
+    }
+  }, [language]);
+
+  const changeLanguage = (selectedLanguage) => {
+    setLanguage(selectedLanguage);
+  };
+  const callfunc = (code)=>{
+    setCode(code);
+  }
+
+  const executeCode = async () => {
+    try {
+      const postData = {
+        title: language,
+        body: code,
+      };
+      const response = await axios.post('http://localhost:3000/code', postData);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="header">Online IDE</div>
+      <div className="control-panel">
+        Select language:
+        <select className="languages" value={language} onChange={(e) => changeLanguage(e.target.value)}>
+          <option value="py">Python</option>
+          <option value="js">Javascript</option>
+          <option value="cpp">C++</option>
+          <option value="typescript">Typescript</option>
+        </select>
+      </div>
+      <AceEditor
+        ref={editorRef}
+        mode={language === 'js' ? 'javascript' : language}
+        theme="dreamweaver"
+        onChange={(newCode)=>callfunc(newCode)}
+        value={code}
+        editorProps={{ $blockScrolling: true }}
+      />
+      <div className="button-container">
+        <button className="btn" onClick={executeCode}>
+          Run
+        </button>
+      </div>
+      <div className="output"></div>
+    </div>
+  );
+}
+
+export default App;
+
